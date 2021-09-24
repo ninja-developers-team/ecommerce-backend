864f132724e897cdfe6c60b605f7b95d8c61bf7f
Saja Malkawi <sajamalkawi95@gmail.com>
2021-09-25 00:31:03 +0300

    auth + profile + add user to database

---
+++ hellper/auth.helper.js
@@ -0,0 +1,19 @@
+
+const jwksClient = require('jwks-rsa');
+
+const JWKSURI = process.env.JWKSURI;
+
+const client = jwksClient({
+    jwksUri: JWKSURI
+});
+
+const getKey = (header, callback) => {
+    client.getSigningKey(header.kid, function (err, key) {
+        const signingKey = key.publicKey || key.rsaPublicKey;
+        callback(null, signingKey);
+    });
+};
+
+module.exports = {
+    getKey
+};
--- models/card.schema.js
+++ models/card.schema.js
@@ -1,10 +1,9 @@
 'use strict'
 const mongoose = require('mongoose')
 
-
 const shoppingCardList = new mongoose.Schema({
     userEmail: { type: 'string', unique: true },
-    shoppingCardItems: { type: 'string' },
+    shoppingCardItems: { type: Array },
 })
 
 module.export = { shoppingCardList }
